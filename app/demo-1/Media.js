64783cdf7e6697d9693d1d00f71013ffc9dcc527
nihalTypeApps <nihal@type-apps.com>
2022-03-04 12:58:29 +0530

    Video and images texture

---
+++ app/demo-1/Media.js
@@ -0,0 +1,199 @@
+import { Mesh, Program, Texture } from "ogl";
+
+import fragment from "./fragment.glsl";
+import vertex from "./vertex.glsl";
+
+export default class {
+  constructor({ element, geometry, gl, height, scene, screen, viewport }) {
+    this.element = element;
+    this.image = this.element.querySelector("img");
+    if (!this.image) {
+      this.video = this.element.querySelector("video");
+    }
+
+    this.extra = 0;
+    this.height = height;
+    this.geometry = geometry;
+    this.gl = gl;
+    this.scene = scene;
+    this.screen = screen;
+    this.viewport = viewport;
+
+    if (this.image) {
+      this.createMesh();
+    }
+    if (this.video) {
+      this.createVideoMesh();
+    }
+    this.createBounds();
+
+    this.onResize();
+  }
+
+  createMesh() {
+    const image = new Image();
+    const texture = new Texture(this.gl, {
+      generateMipmaps: false,
+    });
+
+    image.src = this.image.src;
+    image.onload = (_) => {
+      program.uniforms.uImageSizes.value = [
+        image.naturalWidth,
+        image.naturalHeight,
+      ];
+      texture.image = image;
+    };
+
+    const program = new Program(this.gl, {
+      fragment,
+      vertex,
+      uniforms: {
+        tMap: { value: texture },
+        uPlaneSizes: { value: [0, 0] },
+        uImageSizes: { value: [0, 0] },
+        uViewportSizes: { value: [this.viewport.width, this.viewport.height] },
+        uStrength: { value: 0 },
+      },
+      transparent: true,
+    });
+
+    this.plane = new Mesh(this.gl, {
+      geometry: this.geometry,
+      program,
+    });
+
+    this.plane.setParent(this.scene);
+  }
+
+  createVideoMesh() {
+    this.video.loop = true;
+    this.video.muted = true;
+    this.video.play();
+
+    let videoTexture = new Texture(this.gl, {
+      generateMipmaps: false,
+      width: 1024,
+      height: 512,
+    });
+    videoTexture.needsUpdate = true;
+
+    const videoProgram = new Program(this.gl, {
+      vertex,
+      fragment,
+      uniforms: {
+        tMap: { value: videoTexture },
+        uPlaneSizes: { value: [0, 0] },
+        uImageSizes: { value: [0, 0] },
+        uViewportSizes: { value: [this.viewport.width, this.viewport.height] },
+        uStrength: { value: 0 },
+      },
+      cullFace: null,
+      transparent: true,
+    });
+
+    this.plane = new Mesh(this.gl, {
+      geometry: this.geometry,
+      program: videoProgram,
+    });
+
+    this.plane.setParent(this.scene);
+  }
+
+  createBounds() {
+    this.bounds = this.element.getBoundingClientRect();
+
+    this.updateScale();
+    this.updateX();
+    this.updateY();
+
+    this.plane.program.uniforms.uPlaneSizes.value = [
+      this.plane.scale.x,
+      this.plane.scale.y,
+    ];
+  }
+
+  updateScale() {
+    this.plane.scale.x =
+      (this.viewport.width * this.bounds.width) / this.screen.width;
+    this.plane.scale.y =
+      (this.viewport.height * this.bounds.height) / this.screen.height;
+  }
+
+  updateX(x = 0) {
+    this.plane.position.x =
+      -(this.viewport.width / 2) +
+      this.plane.scale.x / 2 +
+      ((this.bounds.left - x) / this.screen.width) * this.viewport.width;
+  }
+
+  updateY(y = 0) {
+    this.plane.position.y =
+      this.viewport.height / 2 -
+      this.plane.scale.y / 2 -
+      ((this.bounds.top - y) / this.screen.height) * this.viewport.height -
+      this.extra;
+  }
+
+  update(y, direction) {
+    this.updateScale();
+    this.updateX();
+    this.updateY(y.current);
+
+    const planeOffset = this.plane.scale.y / 2;
+    const viewportOffset = this.viewport.height / 2;
+
+    this.isBefore = this.plane.position.y + planeOffset < -viewportOffset;
+    this.isAfter = this.plane.position.y - planeOffset > viewportOffset;
+
+    if (direction === "up" && this.isBefore) {
+      this.extra -= this.height;
+
+      this.isBefore = false;
+      this.isAfter = false;
+    }
+
+    if (direction === "down" && this.isAfter) {
+      this.extra += this.height;
+
+      this.isBefore = false;
+      this.isAfter = false;
+    }
+
+    this.plane.program.uniforms.uStrength.value =
+      ((y.current - y.last) / this.screen.width) * 10;
+
+    // Attach video and/or update texture when video is ready
+    if (this.video) {
+      if (this.video.readyState >= this.video.HAVE_ENOUGH_DATA) {
+        if (!this.plane.program.uniforms.tMap.value.image)
+          this.plane.program.uniforms.tMap.value.image = this.video;
+        this.plane.program.uniforms.tMap.value.needsUpdate = true;
+      }
+    }
+  }
+
+  /**
+   * Events.
+   */
+  onResize(sizes) {
+    this.extra = 0;
+
+    if (sizes) {
+      const { height, screen, viewport } = sizes;
+
+      if (height) this.height = height;
+      if (screen) this.screen = screen;
+      if (viewport) {
+        this.viewport = viewport;
+
+        this.plane.program.uniforms.uViewportSizes.value = [
+          this.viewport.width,
+          this.viewport.height,
+        ];
+      }
+    }
+
+    this.createBounds();
+  }
+}
